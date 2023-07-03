8cc9ed19f8e02db631ae566199b52e86d840194e
ferizoozoo <farhadzohoorz@gmail.com>
2023-07-03 07:43:12 +0330

    Add User Entity

---
+++ CleanArchitecture.Domain/Entities/Base/User.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CleanArchitecture.Domain.Entities
+{
+    public class User
+    {
+        public Guid Id { get; set; }
+        public string Firstname { get; set; }
+        public string Lastname { get; set; }
+    }
+}
