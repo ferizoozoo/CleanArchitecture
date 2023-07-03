4936d875a9759772af77bc8667863b30b93bf7e9
ferizoozoo <farhadzohoorz@gmail.com>
2023-07-03 07:43:47 +0330

    Add Adapters library & MdpAdapter

---
+++ CleanArchitecture.Adapters/Mdp/MdpAdapter.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CleanArchitecture.Adapters.Mdp
+{
+    public class MdpAdapter
+    {
+        public async Task<MdpDataReadModel> ReadAsync()
+        {
+            return await Task.FromResult(new MdpDataReadModel
+            {
+                Stockname = "Hello",
+                Price = 15839.23
+            });
+        }
+    }
+}
